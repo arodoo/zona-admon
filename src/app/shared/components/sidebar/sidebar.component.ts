b446ce8ce53224896de7eb05057273752a46ea47
arodoo <haro.em@hotmail.com>
2024-03-26 22:37:45 -0600

    sidebar buttons protected

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -7,6 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbar } from '@angular/material/toolbar';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -17,13 +18,36 @@ import { MatToolbar } from '@angular/material/toolbar';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+
+  constructor(private authService: AuthService) {
+
+  }
 
   menuItems = [
-    { name: 'Inicio', route: '/admin/dashboard', icon: 'dashboard' },
-    { name: 'Usuarios', route: '/admin/users', icon: 'people' },
-    { name: 'Reportes', route: '/admin/reportes', icon: 'reportes' },
-    { name: 'Registros', route: '/admin/documentos', icon: 'folder_open' },
+    { name: 'Inicio', route: '/admin/dashboard', icon: 'dashboard', visible: true },
+    { name: 'Usuarios', route: '/admin/users', icon: 'people', visible: false }, // Oculto por defecto
+    { name: 'Reportes', route: '/admin/reportes', icon: 'reportes', visible: false }, // Oculto por defecto
+    { name: 'Registros', route: '/admin/documentos', icon: 'folder_open', visible: true },
   ];
 
+  ngOnInit() {
+    this.updateMenuVisibility();
+  }
+
+  updateMenuVisibility() {
+    this.authService.user$.subscribe(user => {
+      if (user) {
+        const canDelete = this.authService.canDelete(user);
+        this.menuItems = this.menuItems.map(item => {
+          if (item.name === 'Usuarios' || item.name === 'Reportes') {
+            return { ...item, visible: canDelete };
+          }
+          return item;
+        }
+        );
+      }
+    }
+    );
+  }
 }
