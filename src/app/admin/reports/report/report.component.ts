a6b01cc2333e73f63820fbb1a8f4d23778169317
arodoo <haro.em@hotmail.com>
2024-04-05 12:50:45 -0600

    Refactor report component HTML and SCSS files

--- src/app/admin/reports/report/report.component.ts
+++ src/app/admin/reports/report/report.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 import { AppTitleComponent } from '../../../shared/components/app-title/app-title.component';
 import { DateFormatPipe } from '../../../shared/pipes/date-format.pipe';
@@ -14,20 +17,40 @@ interface Report {
 @Component({
   selector: 'app-report',
   standalone: true,
-  imports: [CommonModule,AppTitleComponent, DateFormatPipe],
+  imports: [CommonModule,AppTitleComponent, DateFormatPipe, MatTableModule, MatPaginator],
   templateUrl: './report.component.html',
   styleUrl: './report.component.scss'
 })
-export class ReportComponent implements OnInit{
+export class ReportComponent implements OnInit, AfterViewInit{
   $reports: Report[] = [];
-  constructor() { }
+
+  displayedColumns: string[] = ['user', 'date', 'location', 'actions'];
+  dataSource = new MatTableDataSource<Report>(this.$reports);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  constructor(private paginatorIntl: MatPaginatorIntl) {
+    this.paginatorIntl.itemsPerPageLabel = 'Registros por página';
+    this.paginatorIntl.nextPageLabel = 'Siguiente';
+    this.paginatorIntl.previousPageLabel = 'Anterior';
+    this.paginatorIntl.firstPageLabel = 'Primera página';
+    this.paginatorIntl.lastPageLabel = 'Última página';
+   }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.paginator.page.subscribe(() =>{
+      window.scrollTo(0,0);
+    })
+  }
 
   ngOnInit(): void {
     this.generateTestReports();
   }
 
+
+
   async generateTestReports(): Promise<void> {
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 258; i++) {
               newDate: new Date(),
       this.$reports.push({
         user: { name: `Aureliano José Buendía ${i + 1}` },
