1f22ae96dce986bef95c698a78b4934dcb4d9417
arodoo <haro.em@hotmail.com>
2024-07-16 22:32:39 -0600

    feat: Add form for saving data in use-prediction-module.component.html and use-prediction-module.component.ts

--- src/app/admin/data-acces-panel/use-prediction-module/use-prediction-module.component.ts
+++ src/app/admin/data-acces-panel/use-prediction-module/use-prediction-module.component.ts
@@ -3,7 +3,12 @@ import { CommonModule } from '@angular/common';
 import { from } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 import { AuthService } from '../../../core/services/auth.service';
 import { UsersService } from '../../../core/services/users.service';
@@ -11,18 +16,31 @@ import { UsersService } from '../../../core/services/users.service';
 @Component({
   selector: 'app-use-prediction-module',
   standalone: true,
-  imports: [CommonModule, MatCheckboxModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule,
+    MatCheckboxModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule
+  ],
   templateUrl: './use-prediction-module.component.html',
   styleUrl: './use-prediction-module.component.scss'
 })
 export class UsePredictionModuleComponent implements OnInit {
   showInstructions = true;
   dontShowAgain = false;
+  dataForm: FormGroup = new FormGroup({});
 
   constructor(private authService: AuthService,
-    private userService: UsersService) { }
-  
+    private userService: UsersService,
+    private fb: FormBuilder,
+    ) { }
+
   ngOnInit(): void {
+
+    this.dataForm = this.fb.group({
+      municipality: ['', Validators.required],
+      deaths: [0, [Validators.required, Validators.min(0)]],
+      injured: [0, [Validators.required, Validators.min(0)]],
+      population: [0, [Validators.required, Validators.min(0)]]
+    });
+
     from(this.authService.getCurrentUserUid()).pipe(
       switchMap(uid => {
         if (uid) {
@@ -56,4 +74,15 @@ export class UsePredictionModuleComponent implements OnInit {
   closeInstructions(): void {
     this.showInstructions = false;
   }
+
+  saveData(): void {
+    if (this.dataForm?.valid) {
+      const data = this.dataForm.value;
+      console.log(data);
+/*       this.http.post('http://localhost:3000/predict', data).subscribe((response) => {
+        console.log(response);
+      }); */
+    }
+  }
+
 }
