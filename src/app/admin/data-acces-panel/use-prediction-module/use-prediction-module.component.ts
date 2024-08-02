db83b84bf9b2aefcae356908d2afe3bd7adeb883
AlanQT12 <alanquevedotoral@gmail.com>
2024-08-02 14:44:43 -0600

    BACKUP COMMIT 01-08_2024

--- src/app/admin/data-acces-panel/use-prediction-module/use-prediction-module.component.ts
+++ src/app/admin/data-acces-panel/use-prediction-module/use-prediction-module.component.ts
@@ -7,45 +7,61 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, provideNativeDateAdapter } from '@angular/material/core';
 import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'; // Importa HttpClientModule
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 import { AuthService } from '../../../core/services/auth.service';
 import { UsersService } from '../../../core/services/users.service';
 import { PredictionService } from '../../../core/services/prediction.service';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-use-prediction-module',
   standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule,
-    MatCheckboxModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule,  HttpClientModule
+    MatCheckboxModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule, HttpClientModule,
+    MatDatepickerModule, MatNativeDateModule
   ],
   templateUrl: './use-prediction-module.component.html',
   styleUrls: ['./use-prediction-module.component.scss'],
-  providers: [PredictionService] // Asegúrate de proveer tu servicio si es necesario
+  providers: [PredictionService, provideNativeDateAdapter()]
 })
 export class UsePredictionModuleComponent implements OnInit {
   showInstructions = true;
   dontShowAgain = false;
   dataForm!: FormGroup;
   predictionResult!: string;
+  municipios: any[] = [];
+  nextId = 1;
+
+  private fieldMapping: { [key: string]: string } = {
+    municipioNombre: 'municipality',
+    habitantes: 'population',
+    accidentes: 'accidents',
+    muertos: 'deaths',
+    heridos: 'injured',
+    fecha: 'date'
+  };
 
   constructor(private authService: AuthService,
-    private userService: UsersService,
-    private fb: FormBuilder,
-    private predictionService: PredictionService
-    ) { }
+              private userService: UsersService,
+              private fb: FormBuilder,
+              private predictionService: PredictionService,
+              private firestore: AngularFirestore) {}
 
   ngOnInit(): void {
-
     this.dataForm = this.fb.group({
-      municipality: ['', Validators.required],
-      deaths: [0, [Validators.required, Validators.min(0)]],
-      injured: [0, [Validators.required, Validators.min(0)]],
-      population: [0, [Validators.required, Validators.min(0)]],
-      //Para el algoritmo de predicción
-      isSafe: [false,]
-    });
+      id: [''], // Asegúrate de que el control 'id' esté definido aquí
+      municipioNombre: ['', Validators.required],
+      habitantes: [0, [Validators.required, Validators.min(0)]],
+      accidentes: [0, [Validators.required, Validators.min(0)]],
+      muertos: [0, [Validators.required, Validators.min(0)]],
+      heridos: [0, [Validators.required, Validators.min(0)]],
+      fecha: ['', Validators.required]
+    }, { validators: uniqueMunicipioFechaValidator(this.municipios) });
 
     from(this.authService.getCurrentUserUid()).pipe(
       switchMap(uid => {
@@ -84,12 +100,54 @@ export class UsePredictionModuleComponent implements OnInit {
   saveData(): void {
     if (this.dataForm?.valid) {
       const data = this.dataForm.value;
+      const municipio = this.municipios.find(m => m.nombre === data.municipioNombre && m.fecha === data.fecha);
+      if (!municipio) {
+        const newMunicipio = { id: this.nextId++, nombre: data.municipioNombre, fecha: data.fecha };
+        this.municipios.push(newMunicipio);
+        data.municipio = newMunicipio.id;
+        data.municipioNombre = newMunicipio.nombre;
+      } else {
+        data.municipio = municipio.id;
+        data.municipioNombre = municipio.nombre;
+      }
+  
+      const translatedData: { [key: string]: any } = {};
+      for (const key in data) {
+        if (this.fieldMapping[key]) {
+          translatedData[this.fieldMapping[key]] = data[key];
+        } else {
+          translatedData[key] = data[key];
+        }
+      }
+  
+      this.firestore.collection('incidentes_bulkData').add(translatedData)
+        .then(() => {
+          console.log('Datos guardados en Firebase correctamente');
+        })
+        .catch((error) => {
+          console.error('Error al guardar los datos en Firebase:', error);
+        });
+  
       this.predictionService.predict(data).subscribe({
-        next: (prediction) => {
-          if (prediction === 0) {
-            this.predictionResult = 'El municipio es seguro.';
-          } else if (prediction === 1) {
-            this.predictionResult = 'El municipio es peligroso.';
+        next: (htmlResponse) => {
+          console.log('Respuesta del servicio de predicción:', htmlResponse); 
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(htmlResponse, 'text/html');
+          const predictionElements = doc.querySelectorAll('p'); 
+          let predictionFound = false;
+          predictionElements.forEach(predictionElement => {
+            const predictionText = predictionElement.textContent;
+            console.log('Texto de la predicción:', predictionText); 
+            if (predictionText!.includes('seguro') || predictionText!.includes('Seguro')) {
+              this.predictionResult = 'El municipio es seguro.';
+              predictionFound = true;
+            } else if (predictionText!.includes('peligroso') || predictionText!.includes('Peligroso')) {
+              this.predictionResult = 'El municipio es peligroso.';
+              predictionFound = true;
+            }
+          });
+          if (!predictionFound) {
+            this.predictionResult = 'No se pudo determinar la seguridad del municipio.';
           }
         },
         error: (err) => {
@@ -97,6 +155,28 @@ export class UsePredictionModuleComponent implements OnInit {
           this.predictionResult = 'Error al realizar la predicción.';
         }
       });
+    } else {
+      if (this.dataForm.errors?.['municipioFechaDuplicado']) {
+        console.error('El municipio y la fecha ya existen. Por favor, ingrese un municipio y fecha únicos.');
+      } else {
+        console.error('Formulario inválido. Por favor, complete todos los campos requeridos correctamente.');
+      }
     }
   }
+
+  existeMunicipio(nombre: string): boolean {
+    return this.municipios.some(m => m.nombre === nombre);
+  }
+}
+
+// Validador personalizado para verificar la unicidad del municipio y la fecha
+function uniqueMunicipioFechaValidator(municipios: any[]): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const municipioNombre = control.get('municipioNombre')?.value;
+    const fecha = control.get('fecha')?.value;
+
+    const municipioExistente = municipios.find(m => m.nombre === municipioNombre && m.fecha === fecha);
+
+    return municipioExistente ? { municipioFechaDuplicado: true } : null;
+  };
 }
